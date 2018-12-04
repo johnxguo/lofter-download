5200f0f1041ed3d8b686adfe5a6e8d13686604fa
johnxguo <johnxacm@gmail.com>
2018-12-05 03:36:22 +0800

    change to asyncio impl

--- lofter.py
+++ lofter.py
@@ -1,149 +1,165 @@
-#coding:utf-8
-import sys, urllib, requests, gzip, io, re, os, socket, shutil
+#author: johnxguo
+#date: 2018-12-5
 
-#lofter原图是只保存八个月的
-#另外rss页面也可以参考一下，更简单直接，http://t95821704.lofter.com/rss?p=2
+import sys
+import urllib
+import requests
+import io
+import re
+import os
+import shutil
+import asyncio
+from session import Session
 
-socket.setdefaulttimeout(30)
+class LofterDownloader:
+    def __init__(self, target, workpath):
+        self.target = target
+        self.workpath = workpath
+        self.maxTaskNum = 7
+        self.curTaskNum = 0
+        self.totalCount = 0
+        self.doneCount = 0
+        self.targetpath = self.workpath + self.target + '/'
+        self.donelist = []
+        self.initUrls()
+        self.initData()
+        self.initRegular()
+        self.initSession()
+        self.asyncrun(self.start())
 
-regBlogId = 'blogId=(.+?)\"'
-reBlogIdCpl = re.compile(regBlogId)
+    def initUrls(self):
+        self.targetMain  = "http://" + self.target + ".lofter.com"
+        self.targetView  = "http://" + self.target + ".lofter.com/view"
+        self.targetPglst = "http://" + self.target + ".lofter.com/dwr/call/plaincall/ArchiveBean.getArchivePostByTime.dwr"
+        self.albumPrefix = "http://" + self.target + ".lofter.com/post/"
 
-regAlbumId = 'permalink=\"(.+?)\"'
-reAlbumIdCpl = re.compile(regAlbumId)
+    def initRegular(self):
+        self.reBlogIdCpl  = re.compile('blogId=(.+?)\"')
+        self.reAlbumIdCpl = re.compile('permalink=\"(.+?)\"')
+        self.reImgCpl     = re.compile('bigimgsrc=\"(.+?)[\?\"]')
 
-regImg = 'bigimgsrc=\"(.+?)[\?\"]'
-reImgCpl = re.compile(regImg)
+    def initSession(self):
+        headers = {
+            'Accept-Encoding': 'gzip',
+            'Referer': self.targetView
+        }
+        self.session = Session(None, headers)
 
-def getFileName(url):
-    return requests.utils.unquote(url).split('/')[-1]
+    def initData(self):
+        self.data = {
+            'callCount':'1',
+            'scriptSessionId':'187',
+            'c0-scriptName':'ArchiveBean',
+            'c0-methodName':'getArchivePostByTime',
+            'c0-id':'0',
+            'c0-param0':'boolean:false',
+            'c0-param1':'number:1794082',
+            'c0-param2':'number:1543936262219',
+            'c0-param3':'number:5000',
+            'c0-param4':'boolean:false',
+            'batchId':'366472'
+        }
+    
+    def asyncrun(self, future):
+        return asyncio.get_event_loop().run_until_complete(future)
 
-def getUrl(url):
-    doc = requests.get(url)
-    return doc.text
+    async def start(self):
+        if not os.path.exists(self.targetpath):
+            os.mkdir(self.targetpath)
+        print(self.targetpath)
+        
+        self.removeUndone()
 
-def postUrl(url, headers, params):
-    data = urllib.parse.urlencode(params)
-    response = requests.post(url=url, data=params, headers=headers)
-    ret = ''
-    return response.content.decode('utf-8')
-
-def downLoadImage(url, retry):
-    print(imgUrl + "  " + str(retry))
-    try:
-        urllib.request.urlretrieve(imgUrl, albumPath + '_' + getFileName(imgUrl))
-    except IOError as e:
-        if retry > 0:
-            retry = retry - 1;
-            downLoadImage(url, retry)
-        else:
+        mainHtml = await self.get(self.targetMain)
+        idList = re.findall(self.reBlogIdCpl, mainHtml)
+        if (len(idList) == 0):
             return
+        blogId = idList[0]
+        print(blogId)
+
+        self.loadDonelist()
+        self.createUndone()
+
+        doc = await self.post(self.targetPglst, {'c0-param1':'number:' + blogId})
+        albumIdList = re.findall(self.reAlbumIdCpl, doc)
+        print(albumIdList)
+
+        self.totalCount = len(albumIdList)
+        tasks = [asyncio.ensure_future(self.fuckAlbum(albumId)) for albumId in albumIdList]
+        await asyncio.wait(tasks)
             
-def isDone(albumId):
-    for id in doneList:
-        if id == albumId:
-            return True
-    return False
-        
-def loadDoneList():
-    if not os.path.isfile(targetName + "/donelist.txt"):
-        return
-    for line in open(targetName + "/donelist.txt"):
-        doneList.append(line.rstrip('\n'))
+        self.removeUndone()
     
-def createUnDone():
-    f = open(targetName + "/undone", "w")
-    f.write("undone")
-    f.close()
+    async def fuckAlbum(self, albumId):
+        while self.curTaskNum >= self.maxTaskNum:
+            await asyncio.sleep(3)
+        self.curTaskNum += 1
+        albumPath = targetName + "/" + albumId
+        if self.isDone(albumId=albumId):
+            self.doneCount += 1
+            self.curTaskNum -= 1
+            print(albumId + " is done")
+            return
+        albumUrl = self.albumPrefix + albumId
+        albumHtml = await self.get(albumUrl)
+        imgList = re.findall(self.reImgCpl, albumHtml)
+        print(albumId + " 共" + str(len(imgList)) + "张, " + str(self.doneCount) + "/" + str(self.totalCount))
+        if len(imgList) > 0:
+            tasks = [asyncio.ensure_future(self.download(imgUrl, albumPath, 5)) for imgUrl in imgList]
+            await asyncio.wait(tasks)
+            if self.isResultSucc(tasks):
+                self.onDone(albumId=albumId)
+        self.doneCount += 1
+        self.curTaskNum -= 1
+            
+    def isResultSucc(self, tasks):
+        return len([task for task in tasks if not task.result()]) == 0
+
+    def getUrlFileName(self, url):
+        return requests.utils.unquote(url).split('/')[-1]
+
+    async def get(self, url):
+        return await self.session.get(url)
+
+    async def post(self, url, data):
+        data = {**self.data, **data}
+        return await self.session.post(url, data)
+
+    async def download(self, url, albumPath, retry):
+        print(url + "  " + str(retry))
+        path = albumPath + '_' + self.getUrlFileName(url)
+        return await self.session.fetch(url, path)
+            
+    def isDone(self, albumId):
+        for id in self.donelist:
+            if id == albumId:
+                return True
+        return False
         
-def removeUnDone():
-    if os.path.isfile(targetName + "/undone"):
-        os.remove(targetName + "/undone")
+    def loadDonelist(self):
+        self.donelist.clear()
+        if not os.path.isfile(targetName + "/donelist.txt"):
+            return
+        for line in open(targetName + "/donelist.txt"):
+            self.donelist.append(line.rstrip('\n'))
     
-def onDone(albumId):
-    f = open(targetName + "/donelist.txt", "a+")
-    f.writelines(albumId + "\n")
-    f.close()
-    doneList.append(albumId)
-
-#------------------------------------------------------------
-
-params = {
-    'callCount':'1',
-    'scriptSessionId':'187',
-    'c0-scriptName':'ArchiveBean',
-    'c0-methodName':'getArchivePostByTime',
-    'c0-id':'0',
-    'c0-param0':'boolean:false',
-    'c0-param1':'number:1794082',
-    'c0-param2':'number:1543936262219',
-    'c0-param3':'number:5000',
-    'c0-param4':'boolean:false',
-    'batchId':'366472'
-}
-
-headers = { 
-  'Accept-Encoding':'gzip',
-  'Referer':''
-}  
-
-doneList = []
+    def createUndone(self):
+        with open(targetName + "/undone", "w") as f:
+            f.write("undone")
+            
+    def removeUndone(self):
+        if os.path.isfile(targetName + "/undone"):
+            os.remove(targetName + "/undone")
+        
+    def onDone(self, albumId):
+        with open(targetName + "/donelist.txt", "a+") as f:
+            f.writelines(albumId + "\n")
+        self.donelist.append(albumId)
 
 if len(sys.argv) <= 1:
     print("输入目标用户名")
     exit()
 
 targetName = sys.argv[1]
-targetMain = "http://" + targetName + ".lofter.com"
-targetView = "http://" + targetName + ".lofter.com/view"
-targetPageList = "http://" + targetName + ".lofter.com/dwr/call/plaincall/ArchiveBean.getArchivePostByTime.dwr"
-albumPrefix = "http://" + targetName + ".lofter.com/post/"
-headers['Referer'] = targetView
 
-if not os.path.exists(targetName):
-    os.mkdir(targetName)
-print(targetMain)
-
-removeUnDone()
-
-mainHtml = getUrl(targetMain)
-idList = re.findall(reBlogIdCpl, mainHtml)
-if (len(idList) == 0):
-    exit()
-blogId = idList[0]
-print(blogId)
-
-loadDoneList()
-createUnDone()
-
-params['c0-param1'] = 'number:' + blogId
-
-doc = postUrl(targetPageList, headers, params)
-albumIdList = re.findall(reAlbumIdCpl, doc)
-print(albumIdList)
-
-size = len(albumIdList)
-cur = 0
-
-for albumId in albumIdList:
-    print(albumId + "  " + str(cur) + "/" + str(size))
-    cur += 1
-    albumPath = targetName + "/" + albumId
-    if isDone(albumId=albumId):
-        continue
-    albumUrl = albumPrefix + albumId
-    albumHtml = getUrl(albumUrl)
-    imgList = re.findall(reImgCpl, albumHtml)
-    print("共" + str(len(imgList)) + "张")
-    for imgUrl in imgList:
-        downLoadImage(imgUrl, 5)
-    onDone(albumId=albumId)
-    
-removeUnDone()
-    
-    
-    
-    
-    
-    
-    
+LofterDownloader(targetName, './')
